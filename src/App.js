14eee551cfbd7721a387a3754bbde9ba4738abcf
Angela Patrinou <patrinoua@gmail.com>
2022-03-10 00:06:00 +0100

    UI changes

--- src/App.js
+++ src/App.js
@@ -2,38 +2,46 @@ import './App.css'
 import styled from 'styled-components'
 
 const headlineColor = 'white'
+const underlineColor = 'salmon'
 const regularColor = 'white'
+const fontFamilyTitle = 'ui-monospace'
+const fontFamilyText = 'monospace'
+const fontFamilyParagraphText = 'verdana'
 
 const Container = styled.div`
   width: 100vw;
-  min-height: 100vh;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: -webkit-linear-gradient(0.62turn, #ae009e, #031f30);
+`
+const InnerContainer = styled.div`
+  /* background: brown; */
+  /* padding: 50px; */
   display: flex;
   flex-direction: column;
   align-items: center;
-  font-family: monospace;
-  /* justify-content: center; */
-  /* background: -webkit-linear-gradient(0.62turn, #eee, #133); */
-  background: -webkit-linear-gradient(0.62turn, #99a8af, #011010);
 `
 const Title = styled.h1`
   font-size: 8em;
-  margin: 0.4em;
+  margin: 0;
   color: white;
   text-align: center;
+  font-family: ${fontFamilyTitle};
 `
-const Regular = styled.h4`
-  font-size: 1.2em;
+const Regular = styled.p`
+  /* font-size: 1.3em; */
   max-width: 800px;
-  margin: 0.6em;
-  margin-right: 1em;
-  margin-left: 1em;
+  /* margin: 0.6em; */
   text-align: justify;
   color: ${regularColor};
   font-family: verdana;
   font-weight: 100;
+  font-family: ${fontFamilyParagraphText};
 `
 const A = styled.a`
-  text-decoration-color: ${headlineColor};
+  text-decoration-color: ${underlineColor};
 `
 const H2 = styled.h2`
   font-size: 2.5em;
@@ -41,6 +49,7 @@ const H2 = styled.h2`
   letter-spacing: 0.2;
   text-align: center;
   color: ${headlineColor};
+  font-family: ${fontFamilyText};
 `
 const H3 = styled(H2)`
   font-size: 2em;
@@ -49,81 +58,92 @@ const H3 = styled(H2)`
 function App() {
   return (
     <Container>
-      <Title>Work in tech</Title>
-      <Regular>Working in tech is fun!</Regular>
-      <Regular>
-        This is a guide with the most useful resources I've found over the
-        years, including learning material, best practices and tips if you're
-        considering joining tech or currently looking for a new position.
-      </Regular>
-      <Regular>
-        Feel free to reach out to me if you know an interesting link that I
-        could add here, or looking for feedback!
-      </Regular>
-      <Regular>
-        This is a personal project to help you get a first idea about working in
-        tech, inspire you and give you some links to further investigate. No
-        company stands behind this.
-      </Regular>
-      {/* <A href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+      <InnerContainer>
+        <Title>Work in tech</Title>
+        <H3>Working in tech is fun!</H3>
+        <Regular>
+          This is a guide with resources collected over the years, including
+          learning material, best practices and tips if you're considering
+          joining tech or currently looking for a new position.
+        </Regular>
+        <Regular>
+          Feel free to reach out to me if you know an interesting link that I
+          could add here, or looking for feedback!
+        </Regular>
+        <Regular>
+          This is a personal project to help you get a first idea about working
+          in tech, inspire you and give you some links to further investigate.
+          No company stands behind this.
+        </Regular>
+        {/* <A href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
 				<H2>Pros & Cons</H2>
 			</A> */}
-      <A
-        href='https://github.com/patrinoua/tipsforjobs/blob/master/README.md#roadmaps-step-by-step-guides-and-paths-to-learn-different-tools-and-technologies'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <H2>Roadmaps & bootcamps</H2>
-      </A>
-      <A
-        href='https://github.com/patrinoua/tipsforjobs/blob/master/README.md#learning-resources'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <H2>Learning Material</H2>
-      </A>
-      <A
-        href='https://github.com/patrinoua/tipsforjobs/blob/master/README.md#roadmaps-step-by-step-guides-and-paths-to-learn-different-tools-and-technologies'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <H2>Interview Preparation</H2>
-      </A>
-      <Regular>
+        <A
+          href='https://github.com/patrinoua/tipsforjobs/blob/master/README.md#roadmaps-step-by-step-guides-and-paths-to-learn-different-tools-and-technologies'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <H2>> Roadmaps & bootcamps</H2>
+        </A>
+        <A
+          href='https://github.com/patrinoua/tipsforjobs/blob/master/README.md#learning-resources'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <H2>> Learning Material</H2>
+        </A>
+        <A
+          href='https://github.com/patrinoua/tipsforjobs/blob/master/README.md#roadmaps-step-by-step-guides-and-paths-to-learn-different-tools-and-technologies'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <H2>> Interview Preparation</H2>
+        </A>
+        {/* <Regular>
         You can contact me for personalised advice or feedback on your CV, code
         reviews, mock interviews etc.*
-      </Regular>
-      <div
-        style={{
-          display: 'flex',
-          marginRight: '15px',
-          fontSize: '.9em',
-          marginTop: 50,
-        }}
-      >
-        <Regular style={{ marginRight: '10px' }}>Made by </Regular>
-        <a
-          href='https://angelapatrinou.com'
-          target='_blank'
-          rel='noopener noreferrer'
-          style={{ textDecorationColor: 'white' }}
+      </Regular> */}
+        <div
+          style={{
+            display: 'flex',
+            marginRight: '15px',
+            marginTop: 50,
+            display: 'flex',
+            fontSize: '.9em',
+            justifyContent: 'center',
+          }}
         >
-          <Regular>Angela Patrinou,</Regular>
-        </a>
-      </div>
-      <div style={{ display: 'flex', marginRight: '15px', fontSize: '.9em' }}>
-        <Regular style={{ marginRight: '10px' }}>creator of</Regular>
-        <a
-          href='https://angelapatrinou.com'
-          target='_blank'
-          rel='noopener noreferrer'
-          style={{ textDecorationColor: 'white' }}
+          <Regular style={{ marginRight: '10px' }}>Made by </Regular>
+          <a
+            href='https://angelapatrinou.com'
+            target='_blank'
+            rel='noopener noreferrer'
+            style={{ textDecorationColor: 'white' }}
+          >
+            <Regular>Angela Patrinou</Regular>
+          </a>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            fontSize: '.9em',
+            justifyContent: 'center',
+          }}
         >
-          <Regular>Buddies</Regular>
-        </a>
-        <Regular>the expat app.</Regular>
-      </div>
-      <Regular
+          <Regular>creator of</Regular>
+          <a
+            href='https://angelapatrinou.com'
+            target='_blank'
+            rel='noopener noreferrer'
+            style={{ textDecorationColor: 'white' }}
+          >
+            <Regular style={{ marginLeft: '10px', marginRight: '10px' }}>
+              Buddies
+            </Regular>
+          </a>
+          <Regular>the expat app.</Regular>
+        </div>
+        {/* <Regular
         style={{
           alignSelf: 'flex-end',
           marginRight: 20,
@@ -133,7 +153,8 @@ function App() {
         }}
       >
         *Paid Service
-      </Regular>
+      </Regular> */}
+      </InnerContainer>
     </Container>
   )
 }
